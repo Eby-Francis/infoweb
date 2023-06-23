3bfeda4a52c7cfc19a8597f65b7444608034d6dd
Eby-Francis <99521915+Eby-Francis@users.noreply.github.com>
2023-06-24 00:17:07 +0530

    Add files via upload

---
+++ includes/banner.php
@@ -0,0 +1,55 @@
+
+<div class="banner">
+  
+<div class="iconbar">
+  <div class="row">
+    
+    <div class="col-md-3">
+      <div class="row">
+      <h4><i class="fa fa-facebook" aria-hidden="true"></i></h4>
+       <h4><i class="fa fa-twitter" aria-hidden="true"></i></h4>
+        <h4><i class="fa fa-google-plus" aria-hidden="true"></i></h4>
+         <h4><i class="fa fa-linkedin" aria-hidden="true"></i></h4>
+          <h4><i class="fa fa-globe" aria-hidden="true"></i></h4>
+        </div>
+    </div>
+
+
+    <div class="col-md-9">
+      <h3><i class="fa fa-phone" aria-hidden="true"></i>&nbsp; Enquiry : 12345</h3>
+    </div>
+    <div class="underline">
+  </div>
+</div>
+</div>
+
+<div class="menubar">
+  <div class="row">
+    <div class="col-md-3">
+      <h3> Informing</h3>
+    </div>
+    <div class="col-md-9">
+      <ul>
+
+        <li><a href="file:///E:/1%20web/web%203/Contact.html">Contact</a></li>
+        <li><a href="file:///E:/1%20web/web%203/testimonials.html">Testimonials</a></li>
+        <li><a href="file:///E:/1%20web/web%203/Dropdown.html">Dropdown</a></li>
+        <li><a href="file:///E:/1%20web/web%203/about-us.html">About</a></li>
+        <li><a href="#">Home</a></li>
+
+
+      </ul>
+  </div>
+</div>
+
+
+
+</div>
+
+<h1>Start learning for bright future</h1>
+<button>read more</button>
+
+</div>
+<!--------------------------------------------------------------banner-ends--------- -->
+
+ 
